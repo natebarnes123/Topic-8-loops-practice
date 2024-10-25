8311f4774cefc425db41c627f534fb850051ceec
Nathan Barnes <339723215@GoTVDSB.ca>
2024-10-25 09:26:41 -0400

    kiui

--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
-﻿namespace Topic_8_loops_practice
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Topic_8_loops_practice
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             List<string> names = new List<string> { "trinity", "Neo", "morphius", "smith" };
-            Console.WriteLine(names)
+            Console.WriteLine(names[1] + " is 'the one'.");
+            Console.WriteLine(names[2] + " is 'The bad guy'.");
+            Console.WriteLine("There are " + names.Count + " names on my list.");
+            names[3] = "Mr aldworth";
+            Console.WriteLine(names[3] + " is a coding teacher");
+            
 
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(i + ". " + names[i]);
+
+            }
 
         }
     }
